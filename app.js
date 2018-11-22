bf1fa748fe1b5e87358cc8dca4796a598569aece
Adrian Ryan <adrian@innovationlair.com.au>
2018-11-22 22:52:25 +0800

    change of app.js pathing

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
-var request		= require("request");
+
 
 var app = express();
 
@@ -34,10 +34,16 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+// // app.post('/journeybuilder/save/', activity.save );
+// // app.post('/journeybuilder/validate/', activity.validate );
+// // app.post('/journeybuilder/publish/', activity.publish );
+// app.post('/journeybuilder/execute/', activity.execute );
+
+app.post('/save/', activity.save );
+app.post('/validate/', activity.validate );
+app.post('/publish/', activity.publish );
+app.post('/execute/', activity.execute );
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
